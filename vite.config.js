8abe83131728f36e0626951e795ef847daa522bd
RikuDev <riku@rikudev.com>
2023-07-03 19:02:42 +0200

    cambio de dominio

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://rikudev.com/TicTacToe', 
+  base: 'https://tictactoe.rikudev.com', // Configura la ruta base de tu aplicación
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Configura la carpeta de salida para los archivos generados por el comando npm run build
   },
 })
